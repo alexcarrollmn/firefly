6f093cd2bd514e77217a70bd8aaccc7bdb7309ab
Jess Green <jesgs@users.noreply.github.com>
2015-04-02 09:10:04 -0500

    White-space updates (ugh, again)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var Firefly = require('./firefly'),
-	tools   = require('./utilities'),
-	numPixels   = 50;
+    tools   = require('./utilities'),
+    numPixels   = 50;
 
 
 var fireflyTimer = setInterval(runFirefly, 1000); // initial interval, changes while program is running
@@ -10,22 +10,22 @@ var fireflyTimer = setInterval(runFirefly, 1000); // initial interval, changes w
  */
 function runFirefly()
 {
-	if (fireflyTimer) {
-		clearInterval(fireflyTimer);
-	}
-	
-	var interval = tools.getRandomArbitrary(3000, 20000);
-	
+    if (fireflyTimer) {
+        clearInterval(fireflyTimer);
+    }
+    
+    var interval = tools.getRandomArbitrary(3000, 20000);
+    
     // attach parent interval for debugging
     Firefly.prototype.parentInterval = interval
 
-	var pos = Math.round( Math.random() * numPixels),
-		fly = new Firefly(pos);
+    var pos = Math.round( Math.random() * numPixels),
+        fly = new Firefly(pos);
     
-	fly.start();
-	fly.pause();
-	fly.debug();
-		
+    fly.start();
+    fly.pause();
+    fly.debug();
+        
     // start a new timer
     fireflyTimer = setInterval(runFirefly, interval);
 }
